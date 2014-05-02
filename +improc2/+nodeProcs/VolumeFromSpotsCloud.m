4833061a5bd6c14c5d59f09b8aabd86209913487
Gautham Nair <gautham.p.nair@gmail.com>
2014-05-01 23:17:41 -0400

    made volume code work for example data with nuclear mask by removing undersampling correction.

--- +improc2/+nodeProcs/VolumeFromSpotsCloud.m
+++ +improc2/+nodeProcs/VolumeFromSpotsCloud.m
@@ -37,60 +37,62 @@ classdef VolumeFromSpotsCloud < improc2.interfaces.ProcessedData
             x = Js;
             y = Is;
             z = Ks;
+            
+            expFactor = 1;
             [vOrig, cellTopOrig, cellBottomOrig] = ...
                 improc2.volume.findVol_Expand2(croppedMask, nuclearMask, ...
-                x, y, z, 1, pData.radius);
+                x, y, z, expFactor, pData.radius);
             
-            [xf, yf, zf] = improc2.volume.fillVol2(croppedMask, z, cellTopOrig, cellBottomOrig, numel(x), 1);
-            
-            [vFake cellTopFake cellBottomFake] = ...
-                improc2.volume.findVol_Expand2(croppedMask, nuclearMask, ...
-                xf, yf, zf, 1, pData.radius);
-            
-            if vFake < vOrig
-                begInt = 1;
-            else % This is a hack, I don't expect this to happen...
-                disp('fake volume is larger than expected');
-                begInt = 0.5;
-            end
-            
-            expFactor = vOrig/vFake;
-            
-            while true
-                [xf yf zf] = improc2.volume.fillVol2(croppedMask, z, cellTopOrig, cellBottomOrig, numel(x), expFactor);
-                
-                [vFake cellTopFake cellBottomFake] = improc2.volume.findVol_Expand2(croppedMask, ...
-                    nuclearMask, xf, yf, zf, expFactor, pData.radius);
-                
-                if vFake > vOrig
-                    endInt = expFactor;
-                    break;
-                else
-                    expFactor = 1.1*expFactor;
-                end
-            end
-            
-            
-            expFactor = (begInt + endInt)/2;
-            while true
-                [xf, yf, zf] = improc2.volume.fillVol2(croppedMask, z, ...
-                    cellTopOrig, cellBottomOrig, numel(x), expFactor);
-                
-                [vFake, cellTopFake, cellBottomFake] = ...
-                    improc2.volume.findVol_Expand2(croppedMask, nuclearMask, ...
-                    xf, yf, zf, expFactor, pData.radius);
-                
-                if abs(1-vOrig/vFake)<0.01
-                    break;
-                elseif vOrig/vFake > 1 %this means our expFactor is too small
-                    begInt = expFactor; %make this the beginning of our interval
-                    expFactor = (endInt+begInt)/2; %increase expFactor
-                else %this means our expFactor is too large
-                    endInt = expFactor; %make this the end of the interval
-                    expFactor = (endInt+begInt)/2;
-                end
-            end
-            
+%             [xf, yf, zf] = improc2.volume.fillVol2(croppedMask, z, cellTopOrig, cellBottomOrig, numel(x), 1);
+%             
+%             [vFake cellTopFake cellBottomFake] = ...
+%                 improc2.volume.findVol_Expand2(croppedMask, nuclearMask, ...
+%                 xf, yf, zf, 1, pData.radius);
+%             
+%             if vFake < vOrig
+%                 begInt = 1;
+%             else % This is a hack, I don't expect this to happen...
+%                 disp('fake volume is larger than expected');
+%                 begInt = 0.5;
+%             end
+%             
+%             expFactor = vOrig/vFake;
+%             
+%             while true
+%                 [xf yf zf] = improc2.volume.fillVol2(croppedMask, z, cellTopOrig, cellBottomOrig, numel(x), expFactor);
+%                 
+%                 [vFake cellTopFake cellBottomFake] = improc2.volume.findVol_Expand2(croppedMask, ...
+%                     nuclearMask, xf, yf, zf, expFactor, pData.radius);
+%                 
+%                 if vFake > vOrig
+%                     endInt = expFactor;
+%                     break;
+%                 else
+%                     expFactor = 1.1*expFactor;
+%                 end
+%             end
+%             
+%             
+%             expFactor = (begInt + endInt)/2;
+%             while true
+%                 [xf, yf, zf] = improc2.volume.fillVol2(croppedMask, z, ...
+%                     cellTopOrig, cellBottomOrig, numel(x), expFactor);
+%                 
+%                 [vFake, cellTopFake, cellBottomFake] = ...
+%                     improc2.volume.findVol_Expand2(croppedMask, nuclearMask, ...
+%                     xf, yf, zf, expFactor, pData.radius);
+%                 
+%                 if abs(1-vOrig/vFake)<0.01
+%                     break;
+%                 elseif vOrig/vFake > 1 %this means our expFactor is too small
+%                     begInt = expFactor; %make this the beginning of our interval
+%                     expFactor = (endInt+begInt)/2; %increase expFactor
+%                 else %this means our expFactor is too large
+%                     endInt = expFactor; %make this the end of the interval
+%                     expFactor = (endInt+begInt)/2;
+%                 end
+%             end
+%             
             cellTopReal = imresize(cellTopOrig,expFactor);
             cellTopReal = cellTopReal*expFactor;
             cellBottomReal = imresize(cellBottomOrig,expFactor);
--- +improc2/+nodeProcs/VolumeFromSpotsCloud_test.m
+++ +improc2/+nodeProcs/VolumeFromSpotsCloud_test.m
@@ -7,12 +7,12 @@ tools = improc2.launchImageObjectBrowsingTools(collection);
 mockMaskContainer = struct();
 mockMaskContainer.mask = tools.objectHandle.getCroppedMask();
 
-%dapiProcData = tools.objectHandle.getProcessorData('dapi');
-fakeNoNuclearMaskContainer = struct();
-fakeNoNuclearMaskContainer.mask = false(size(mockMaskContainer.mask));
+dapiProcData = tools.objectHandle.getProcessorData('dapi');
+%fakeNoNuclearMaskContainer = struct();
+%fakeNoNuclearMaskContainer.mask = false(size(mockMaskContainer.mask));
 
 cySpotsData = tools.objectHandle.getProcessorData('cy');
 
 planeSpacing = 0.35;
 x = improc2.nodeProcs.VolumeFromSpotsCloud(planeSpacing);
-xProcessed = run(x, mockMaskContainer, fakeNoNuclearMaskContainer, cySpotsData);
+xProcessed = run(x, mockMaskContainer, dapiProcData, cySpotsData);
