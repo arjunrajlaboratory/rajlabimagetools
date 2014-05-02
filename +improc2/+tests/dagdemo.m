9ea202e11d51b8d6e8de257f826eea6b7de29865
Gautham Nair <gautham.p.nair@gmail.com>
2014-05-02 14:03:28 -0400

    a more full DAG demo

--- +improc2/+tests/dagdemo.m
+++ +improc2/+tests/dagdemo.m
@@ -48,7 +48,6 @@ improc2.processing.updateAll(collection);
 dataAdder = improc2.processing.DataAdder(collection);
 dataAdder.view();
 %%
-dataAdder.addDataToObject(improc2.nodeProcs.TwoStageSpotFitProcessedData, 'cy', 'cy:Fitted')
 dataAdder.addDataToObject(improc2.nodeProcs.TwoStageSpotFitProcessedData, 'tmr', 'tmr:Fitted')
 dataAdder.view();
 %%
@@ -57,29 +56,53 @@ dataAdder.repeatForAllObjectsAndQuit();
 improc2.processing.updateAll(collection);
 %%
 
+tools = improc2.launchImageObjectBrowsingTools(collection);
+tools.objectHandle.view();
 
+%%
+improc2.launchThresholdGUI(collection);
 
+%%
+tools.refresh();
+tools.navigator.tryToGoToArray(1);
+tools.navigator.tryToGoToObj(1);
+tools.objectHandle.view()
+%%
+
+tools.navigator.tryToGoToObj(2);
+tools.objectHandle.view()
+%%
+
+improc2.processing.updateAll(collection);
+%%
+tools.refresh();
+tools.navigator.tryToGoToArray(1);
+tools.navigator.tryToGoToObj(1);
+tools.objectHandle.view()
+tools.navigator.tryToGoToObj(2);
+tools.objectHandle.view()
+
+%% getting:
+
+cySpots = tools.objectHandle.getProcessorData('cy');
+volProc = tools.objectHandle.getProcessorData(...
+    'imageObject', 'improc2.nodeProcs.VolumeFromSpotsCloud');
 
 
 %% add threshold QC
 
+
+
+
 dataAdder = improc2.processing.DataAdder(collection);
 dataAdder.view();
 %%
-dataAdder.addDataToObject(improc2.nodeProcs.ThresholdQCData(), 'cy', 'cy:ThreshQC')
-dataAdder.addDataToObject(improc2.nodeProcs.ThresholdQCData(), 'tmr', 'tmr:ThreshQC')
-dataAdder.addDataToObject(improc2.nodeProcs.ThresholdQCData(), 'alexa', 'alexa:ThreshQC')
+dataAdder.addDataToObject(improc2.nodeProcs.ThresholdQCData(), 'cy:Spots', 'cy:ThreshQC')
+dataAdder.addDataToObject(improc2.nodeProcs.ThresholdQCData(), 'tmr:Spots', 'tmr:ThreshQC')
+dataAdder.addDataToObject(improc2.nodeProcs.ThresholdQCData(), 'alexa:Spots', 'alexa:ThreshQC')
 dataAdder.view();
 %%
 dataAdder.repeatForAllObjectsAndQuit();
 
 %%
 improc2.processing.updateAll(collection)
-
-
-
-
-
-
-tools = improc2.launchImageObjectBrowsingTools(collection);
-tools.objectHandle.view()
