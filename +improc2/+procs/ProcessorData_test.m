e2644ccb924c40d0026f758060826fa75d3cc829
Gautham Nair <gautham.p.nair@gmail.com>
2014-05-14 15:25:21 -0400

    removed most entities related to the non-DAG improc2.ImageObject

--- +improc2/+procs/ProcessorData_test.m
+++
@@ -1,6 +0,0 @@
-improc2.tests.cleanupForTests;
-
-x = improc2.procs.ProcessorData();
-assert(~x.isProcessed)
-y = run(x);
-assert(y.isProcessed)
--- +improc2/RemembersRunAndDataChange_test.m
+++
@@ -1,6 +0,0 @@
-improc2.tests.cleanupForTests;
-
-x = improc2.RemembersRunAndDataChange();
-assert(~x.isProcessed)
-x = x.run();
-assert(x.isProcessed)
