ae46ce0f7ec8eaf5e14fa266a51baa592bac1a30
Gautham Nair <gautham.p.nair@gmail.com>
2014-05-23 10:09:36 -0400

    graph based object appears as though it has an object_mask property

--- +improc2/+dataNodes/GraphBasedImageObject.m
+++ +improc2/+dataNodes/GraphBasedImageObject.m
@@ -6,9 +6,22 @@ classdef GraphBasedImageObject
         metadata = struct();
     end
     
+    properties (Dependent = true)
+        object_mask
+    end
+    
     methods
         function p = GraphBasedImageObject()
             p.annotations = struct('isGood', improc2.TypeCheckedLogical(true));
+        end
+        
+        function object_mask = get.object_mask(p)
+            object_mask = p.graph.nodes{1}.data;
+        end
+        
+        function p = set.object_mask(p, object_mask)
+            assert(isa(object_mask, 'improc2.dataNodes.ImageObjectBaseData'))
+            p.graph.nodes{1}.data = object_mask;
         end
     end
 end
