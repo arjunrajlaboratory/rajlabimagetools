36a493d4867ade787a9b215010644e9f494a1db4
Allison Cote <coteaj@gmail.com>
2015-11-20 08:10:56 -0500

    modified processImageObjects_expansion to run aTrous with different default parameters and have different default node names

--- +improc2/processImageObjects.m
+++ +improc2/processImageObjects.m
@@ -1,14 +1,23 @@
-function processImageObjects(dirPathOrAnArrayCollection, channelsToProcess)
+function processImageObjects(varargin)
     
-    if nargin < 1
-        dirPathOrAnArrayCollection = pwd;
-    end
+     ip = inputParser;
+     ip.addOptional('dirPathOrAnArrayCollection', pwd);
+     ip.addOptional('channelsToProcess', {});
+     ip.addParameter('filterParams', struct('sigma',0.5,'numLevels',3));
+     ip.parse(varargin{:});
+
+    dirPathOrAnArrayCollection = ip.Results.dirPathOrAnArrayCollection;
     
     dataAdder = improc2.processing.DataAdder(dirPathOrAnArrayCollection);
     
-    if nargin < 2
+    channelsToProcess = cellstr(ip.Results.channelsToProcess);
+    
+    filterParams = ip.Results.filterParams;
+    
+    if isempty(channelsToProcess)
         channelsToProcess = dataAdder.channelNames;
     end
+    
     
     assert(all(ismember(channelsToProcess, dataAdder.channelNames)), ...
         'At least one of the channels requested to process does not exist')
@@ -16,7 +25,7 @@ function processImageObjects(dirPathOrAnArrayCollection, channelsToProcess)
     fprintf('** Adding unprocessed data templates ...')
     for i = 1:length(channelsToProcess)
         channelName = channelsToProcess{i};
-        [processorData, nodeLabel] = chooseProcessorDataForChannel(channelName);
+        [processorData, nodeLabel] = chooseProcessorDataForChannel(channelName, filterParams);
         dataAdder.addDataToObject(processorData, channelName, nodeLabel)
         if isa(processorData, 'improc2.nodeProcs.aTrousRegionalMaxProcessedData')
             qcData = improc2.nodeProcs.ThresholdQCData();
@@ -31,7 +40,7 @@ function processImageObjects(dirPathOrAnArrayCollection, channelsToProcess)
     improc2.processing.updateAll(dirPathOrAnArrayCollection);
 end
 
-function [processorData, label] = chooseProcessorDataForChannel(channelName)
+function [processorData, label] = chooseProcessorDataForChannel(channelName, filterParams)
     switch channelName
         case 'trans'
             processorData = improc2.nodeProcs.TransProcessedData();
@@ -40,7 +49,7 @@ function [processorData, label] = chooseProcessorDataForChannel(channelName)
             processorData = improc2.nodeProcs.DapiProcessedData();
             label = 'dapiProc';
         otherwise
-            processorData = improc2.nodeProcs.aTrousRegionalMaxProcessedData();
+            processorData = improc2.nodeProcs.aTrousRegionalMaxProcessedData('filterParams', filterParams);
             label = [channelName, ':Spots'];
     end
 end
--- +improc2/processImageObjects_expansion.m
+++ +improc2/processImageObjects_expansion.m
@@ -1,14 +1,23 @@
-function processImageObjects_expansion(dirPathOrAnArrayCollection, channelsToProcess)
+function processImageObjects_expansion(varargin)
     
-    if nargin < 1
-        dirPathOrAnArrayCollection = pwd;
-    end
-    
+     ip = inputParser;
+     ip.addOptional('dirPathOrAnArrayCollection', pwd);
+     ip.addOptional('channelsToProcess', {});
+     ip.addOptional('node_name',':Spots_Expansion') 
+     ip.addParameter('filterParams', struct('sigma',1,'numLevels',4));
+     ip.parse(varargin{:});
+
+    dirPathOrAnArrayCollection = ip.Results.dirPathOrAnArrayCollection;
     dataAdder = improc2.processing.DataAdder(dirPathOrAnArrayCollection);
+    channelsToProcess = cellstr(ip.Results.channelsToProcess);
+    node_name = ip.Results.node_name;
     
-    if nargin < 2
+    filterParams = ip.Results.filterParams;
+    
+    if isempty(channelsToProcess)
         channelsToProcess = dataAdder.channelNames;
     end
+    
     
     assert(all(ismember(channelsToProcess, dataAdder.channelNames)), ...
         'At least one of the channels requested to process does not exist')
@@ -16,12 +25,13 @@ function processImageObjects_expansion(dirPathOrAnArrayCollection, channelsToPro
     fprintf('** Adding unprocessed data templates ...')
     for i = 1:length(channelsToProcess)
         channelName = channelsToProcess{i};
-        [processorData, nodeLabel] = chooseProcessorDataForChannel(channelName);
+        [processorData, nodeLabel] = chooseProcessorDataForChannel(channelName, filterParams, node_name);
         dataAdder.addDataToObject(processorData, channelName, nodeLabel)
-        if isa(processorData, 'improc2.nodeProcs.aTrousRegionalMaxProcessedData_expansion')
+        if strcmp(nodeLabel, [channelName, node_name]) 
+%             isa(processorData, 'improc2.nodeProcs.aTrousRegionalMaxProcessedData')
             qcData = improc2.nodeProcs.ThresholdQCData();
-            qcLabel = [channelName, ':threshQC_expansion'];
-            dataAdder.addDataToObject(qcData, [channelName,':Expansion_Spots'], qcLabel)
+            qcLabel = [channelName, ':threshQC', node_name];
+            dataAdder.addDataToObject(qcData, nodeLabel, qcLabel)
         end
     end
     
@@ -31,7 +41,7 @@ function processImageObjects_expansion(dirPathOrAnArrayCollection, channelsToPro
     improc2.processing.updateAll(dirPathOrAnArrayCollection);
 end
 
-function [processorData, label] = chooseProcessorDataForChannel(channelName)
+function [processorData, label] = chooseProcessorDataForChannel(channelName, filterParams, node_name)
     switch channelName
         case 'trans'
             processorData = improc2.nodeProcs.TransProcessedData();
@@ -40,7 +50,8 @@ function [processorData, label] = chooseProcessorDataForChannel(channelName)
             processorData = improc2.nodeProcs.DapiProcessedData();
             label = 'dapiProc';
         otherwise
-            processorData = improc2.nodeProcs.aTrousRegionalMaxProcessedData_expansion();
-            label = [channelName, ':Expansion_Spots'];
+            processorData = improc2.nodeProcs.aTrousRegionalMaxProcessedData('filterParams', filterParams);
+            label = [channelName, node_name];
+            disp(label)
     end
 end
