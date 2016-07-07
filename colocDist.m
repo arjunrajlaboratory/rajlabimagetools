06865d872482e2141bb1f4e8cb69b9d4d9295ae1
Ian Mellis <ian.mellis@gmail.com>
2016-07-07 10:58:37 -0400

    Fix boundary case of zero SNPs in snpPositions_shifted being fed into colocDist. Now can handle 0-SNP spot cases.

--- colocDist.m
+++ colocDist.m
@@ -16,35 +16,44 @@ Yz = YY(:,3);
 nx = size(X, 1);
 ny = size(Y, 1);
 
-% loop through each SNP
-for i = 1:ny
-    
-    % calculate pairwise distances of all guides with this SNP in XY
-    dsq = zeros(nx,1);
-    
-    for q = 1:2
-        
-        dsq = dsq + (X(:,q) - Y(i,q)).^2;
-    
-    end
-    
-    dsq = sqrt(dsq);
-    
-    % filter for only those within zAllow planes
-    zDiffs = abs(Xz - Yz(i,1));
-    
-%     disp(['zAllow: ', num2str(zAllow), ' size: ', num2str(size(zAllow))])
-    
-%     zAllowVec = zAllow * ones(size(zDiffs));
-    
-    dsq(zDiffs > zAllow, 1) = 1024^2;
-    
-    
-    % store
-    D(:,i) = dsq;
-    
-end
+% check size of SNP spots container and return empty pairwise distances
+% matrix if no SNP spots (similar guide scenario already accounted for in
+% the "dsq = zeros(nx,1);" line below).
+if ny == 0
 
+    D = zeros(nx,0);
+
+else
+    
+    % loop through each SNP
+    for i = 1:ny
+        
+        % calculate pairwise distances of all guides with this SNP in XY
+        dsq = zeros(nx,1);
+        
+        for q = 1:2
+            
+            dsq = dsq + (X(:,q) - Y(i,q)).^2;
+            
+        end
+        
+        dsq = sqrt(dsq);
+        
+        % filter for only those within zAllow planes
+        zDiffs = abs(Xz - Yz(i,1));
+        
+        %     disp(['zAllow: ', num2str(zAllow), ' size: ', num2str(size(zAllow))])
+        
+        %     zAllowVec = zAllow * ones(size(zDiffs));
+        
+        dsq(zDiffs > zAllow, 1) = 1024^2;
+        
+        
+        % store
+        D(:,i) = dsq;
+        
+    end
+end
 
 
 
