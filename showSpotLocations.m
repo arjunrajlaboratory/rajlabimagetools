4e564de2a7f8df912e173bd2cb3cd03bf0170957
Ian Mellis <ian.mellis@gmail.com>
2016-08-26 15:28:14 -0400

    Fixes boundary case for all spotInspectors (including 2- and 3-channel) of a channel with zero spots

--- showSpotLocations.m
+++ showSpotLocations.m
@@ -5,14 +5,15 @@ function showSpotLocations(spots, z, colMarker)
 
 % zcoords = spots(:,3)/zDeform;
 % zcoords = spots(:,3) / (zStepSize/xyPixelDistance);
-zcoords = spots(:,3);
-
-brightspots = spots(find(zcoords == z),:);
-dimspots    = spots(find(zcoords == (z-1) | zcoords == (z+1)),:);
-% 
-% hold off;
-% imshow(imslice,[]);
-% hold on;
-plot(   dimspots(:,1),    dimspots(:,2), colMarker,'markersize',20)
-plot(brightspots(:,1), brightspots(:,2), colMarker,'markersize',20,'LineWidth',3)
-
+if ~isempty(spots)
+    zcoords = spots(:,3);
+    
+    brightspots = spots(find(zcoords == z),:);
+    dimspots    = spots(find(zcoords == (z-1) | zcoords == (z+1)),:);
+    %
+    % hold off;
+    % imshow(imslice,[]);
+    % hold on;
+    plot(   dimspots(:,1),    dimspots(:,2), colMarker,'markersize',20)
+    plot(brightspots(:,1), brightspots(:,2), colMarker,'markersize',20,'LineWidth',3)
+end
