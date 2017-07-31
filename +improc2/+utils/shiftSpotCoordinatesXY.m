59a9e20c251cfcb48b41e88004b0c7354089ae0a
Arjun Raj <arjunrajlab@gmail.com>
2017-07-31 15:00:43 -0400

    Oops put file in wrong branch.

--- +improc2/+utils/shiftSpotCoordinatesXY.m
+++
@@ -1,7 +0,0 @@
-function [outX, outY] = shiftSpotCoordinatesXY(X,Y,fromChannel,toChannel,filename)
-
-shiftTable = readtable(filename,'ReadRowNames',true);
-
-outX = X + shiftTable{'x',[fromChannel toChannel]};
-outY = Y + shiftTable{'y',[fromChannel toChannel]};
-
